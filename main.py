eb164be912c4d06ad269a67b8dde3ca69952fff7
alizpr <aliprheydari@gmail.com>
2022-06-07 11:42:30 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 
 token = "OTgwMzc0ODU3NjEzMTkzMjE2.Gr9iJM.AXG996RSNRIvranPhRk8hxTE8oqezfViY5WUxQ"
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix="-", intents=intents)
+bot = commands.Bot(command_prefix="->", intents=intents)
 
 
 @bot.event
@@ -13,7 +13,7 @@ async def on_ready():
 
 
 @bot.command()
-async def dm_on(ctx, *, msg=None):
+async def dm_on_msg(ctx, *, msg=None):
     sendedmsg = []
     online_members = []
     offline_members = []
@@ -49,7 +49,7 @@ async def dm_on(ctx, *, msg=None):
 
 
 @bot.command()
-async def dm_off(ctx, *, msg=None):
+async def dm_off_msg(ctx, *, msg=None):
     sendedmsg = []
     online_members = []
     offline_members = []
@@ -85,7 +85,7 @@ async def dm_off(ctx, *, msg=None):
 
 
 @bot.command()
-async def dm_all(ctx, *, msg=None):
+async def dm_all_msg(ctx, *, msg=None):
     sendedmsg = []
     all_mem = []
     for member in ctx.guild.members:
