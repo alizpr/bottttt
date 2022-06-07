0f7857ec93208b96207221baca7fb47d9091c61b
alizpr <aliprheydari@gmail.com>
2022-06-07 12:35:04 +0330

    Update main.py

--- main.py
+++ main.py
@@ -16,124 +16,131 @@ async def on_ready():
 
 
 @bot.command()
-@commands.has_permissions(administrator=True)
 async def dm_on_msg(ctx, *, msg=None):
-    sendedmsg = []
-    online_members = []
-    offline_members = []
-    for member in ctx.guild.members:
-        if member.status is not discord.Status.offline:
-            online_members.append(member.name)
-        else:
-            offline_members.append(member.name)
+    if ctx.author.id == 830415923620872213:
+        sendedmsg = []
+        online_members = []
+        offline_members = []
+        for member in ctx.guild.members:
+            if member.status is not discord.Status.offline:
+                online_members.append(member.name)
+            else:
+                offline_members.append(member.name)
 
-    members = ctx.guild.members
-    if msg != None:
-        if members.name in online_members:
-            for member in members:
-                mesg = member.mention + "\n\n\n\n" + msg
-                try:
-                    await member.send(mesg)
-                    print(f"Sended Message | {member.name}")
-                    sendedmsg.append(member.mention)
-                    await ctx.channel.send(("Sended to  " + member.mention) + f" | nafar = {len(sendedmsg)}")
-                except:
-                    print(f"Cloudnt Send Message To | {member.name}")
-                time.sleep(16)
+        members = ctx.guild.members
+        if msg != None:
+            if members.name in online_members:
+                for member in members:
+                    mesg = member.mention + "\n\n\n\n" + msg
+                    try:
+                        await member.send(mesg)
+                        print(f"Sended Message | {member.name}")
+                        sendedmsg.append(member.mention)
+                        await ctx.channel.send(("Sended to  " + member.mention) + f" | nafar = {len(sendedmsg)}")
+                    except:
+                        print(f"Cloudnt Send Message To | {member.name}")
+                    time.sleep(16)
 
-            embed = discord.Embed(
-                    title='list of members',
-                    colour=discord.Colour.purple()
-                    )
-            embed.add_field(name=f"total = {len(sendedmsg)}",value= sendedmsg,inline=False)
-            await ctx.channel.send(embed=embed)
+                embed = discord.Embed(
+                        title='list of members',
+                        colour=discord.Colour.purple()
+                        )
+                embed.add_field(name=f"total = {len(sendedmsg)}",value= sendedmsg,inline=False)
+                await ctx.channel.send(embed=embed)
 
-        else:
-            await ctx.send("ye msg bego man beheshon migam")
+            else:
+                await ctx.send("ye msg bego man beheshon migam")
+    else:
+        await ctx.send("to nemitooni zoor nazan")
 
 
 @bot.command()
-@commands.has_permissions(administrator=True)
 async def dm_off_msg(ctx, *, msg=None):
-    sendedmsg = []
-    online_members = []
-    offline_members = []
-    for member in ctx.guild.members:
-        if member.status is not discord.Status.offline:
-            online_members.append(member.name)
-        else:
-            offline_members.append(member.name)
+    if ctx.author.id == 830415923620872213:
+        sendedmsg = []
+        online_members = []
+        offline_members = []
+        for member in ctx.guild.members:
+            if member.status is not discord.Status.offline:
+                online_members.append(member.name)
+            else:
+                offline_members.append(member.name)
 
-    members = ctx.guild.members
-    if msg != None:
-        if members.name in offline_members:
-            for member in members:
-                mesg = member.mention + "\n\n\n\n" + msg
-                try:
-                    await member.send(mesg)
-                    print(f"Sended Message | {member.name}")
-                    sendedmsg.append(member.mention)
-                    await ctx.channel.send(("Sended to  " + member.mention) + f" | nafar = {len(sendedmsg)}")
-                except:
-                    print(f"Cloudnt Send Message To | {member.name}")
-                time.sleep(16)
+        members = ctx.guild.members
+        if msg != None:
+            if members.name in offline_members:
+                for member in members:
+                    mesg = member.mention + "\n\n\n\n" + msg
+                    try:
+                        await member.send(mesg)
+                        print(f"Sended Message | {member.name}")
+                        sendedmsg.append(member.mention)
+                        await ctx.channel.send(("Sended to  " + member.mention) + f" | nafar = {len(sendedmsg)}")
+                    except:
+                        print(f"Cloudnt Send Message To | {member.name}")
+                    time.sleep(16)
 
-            embed = discord.Embed(
-                    title='list of members',
-                    colour=discord.Colour.purple()
-                    )
-            embed.add_field(name=f"total = {len(sendedmsg)}",value= sendedmsg,inline=False)
-            await ctx.channel.send(embed=embed)
+                embed = discord.Embed(
+                        title='list of members',
+                        colour=discord.Colour.purple()
+                        )
+                embed.add_field(name=f"total = {len(sendedmsg)}",value= sendedmsg,inline=False)
+                await ctx.channel.send(embed=embed)
 
-        else:
-            await ctx.send("ye msg bego man beheshon migam")
+            else:
+                await ctx.send("ye msg bego man beheshon migam")
+    else:
+        await ctx.send("to nemitooni zoor nazan")
 
 
 @bot.command()
-@commands.has_permissions(administrator=True)
 async def dm_all_msg(ctx, *, msg=None):
-    sendedmsg = []
-    all_mem = []
-    for member in ctx.guild.members:
-        if member.status is not discord.Status.offline:
-            all_mem.append(member.name)
-        else:
-            all_mem.append(member.name)
+    if ctx.author.id == 830415923620872213:
+        sendedmsg = []
+        all_mem = []
+        for member in ctx.guild.members:
+            if member.status is not discord.Status.offline:
+                all_mem.append(member.name)
+            else:
+                all_mem.append(member.name)
 
-    members = ctx.guild.members
-    if msg != None:
-        if members.name in all_mem:
-            for member in members:
-                mesg = member.mention + "\n\n\n\n" + msg
-                try:
-                    await member.send(mesg)
-                    print(f"Sended Message | {member.name}")
-                    sendedmsg.append(member.mention)
-                    await ctx.channel.send(("Sended to  " + member.mention) + f" | nafar = {len(sendedmsg)}")
-                except:
-                    print(f"Cloudnt Send Message To | {member.name}")
-                time.sleep(16)
+        members = ctx.guild.members
+        if msg != None:
+            if members.name in all_mem:
+                for member in members:
+                    mesg = member.mention + "\n\n\n\n" + msg
+                    try:
+                        await member.send(mesg)
+                        print(f"Sended Message | {member.name}")
+                        sendedmsg.append(member.mention)
+                        await ctx.channel.send(("Sended to  " + member.mention) + f" | nafar = {len(sendedmsg)}")
+                    except:
+                        print(f"Cloudnt Send Message To | {member.name}")
+                    time.sleep(16)
 
-            embed = discord.Embed(
-                    title='list of members',
-                    colour=discord.Colour.purple()
-                    )
-            embed.add_field(name=f"total = {len(sendedmsg)}",value= sendedmsg,inline=False)
-            await ctx.channel.send(embed=embed)
+                embed = discord.Embed(
+                        title='list of members',
+                        colour=discord.Colour.purple()
+                        )
+                embed.add_field(name=f"total = {len(sendedmsg)}",value= sendedmsg,inline=False)
+                await ctx.channel.send(embed=embed)
 
-        else:
-            await ctx.send("ye msg bego man beheshon migam")
+            else:
+                await ctx.send("ye msg bego man beheshon migam")
+    else:
+        await ctx.send("to nemitooni zoor nazan")
 
 
 @bot.command()
-@commands.has_permissions(administrator=True)
 async def join(ctx):
-    channel = ctx.author.voice.channel
-    await channel.connect()
+    if ctx.author.id == 830415923620872213:
+        channel = ctx.author.voice.channel
+        await channel.connect()
 @bot.command()
-@commands.has_permissions(administrator=True)
 async def leave(ctx):
-    await ctx.voice_client.disconnect()
-
+    if ctx.author.id == 830415923620872213:
+        await ctx.voice_client.disconnect()
+    else:
+        await ctx.send("to nemitooni zoor nazan")
 
 bot.run(token)
