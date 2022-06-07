65dd3e8b836b6a8ead78456d4ad46d78ad3d7bc7
alizpr <aliprheydari@gmail.com>
2022-06-07 13:28:28 +0330

    Update main.py

--- main.py
+++ main.py
@@ -156,12 +156,6 @@ async def dm_user_msg(ctx, users: discord.User, *, message=None):
 
 
 @bot.command()
-async def clear(ctx, amount=500):
-    if ctx.author.id == 830415923620872213:
-        await ctx.channel.purge(limit=amount+1)
-
-
-@bot.command()
 @commands.has_permissions(manage_messages=True)
 async def clear(ctx, amount=None):
     if amount == None:
