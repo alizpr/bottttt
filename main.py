dc34faaace9a2ad3e9110d8c1de418f7f22d6aea
alizpr <aliprheydari@gmail.com>
2022-06-07 12:17:11 +0330

    da

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="->", intents=intents)
 
 
+bot.remove_command('help')
+
+
 @bot.event
 async def on_ready():
     print('Online shode')
